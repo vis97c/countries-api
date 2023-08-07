20e790e7f128589e51ff046c8cb5d74736468d4d
Victor Saa <vis97c@hotmail.com>
2023-08-07 10:46:12 -0500

    rate limit requests

---
+++ src/access.ts
@@ -0,0 +1,98 @@
+interface iIP {
+	blockUntil: number;
+	accessTimes: number[];
+}
+
+export default class AccessLogger {
+	private requests: Record<string, iIP> = {};
+
+	/**
+	 * Access logger constructor
+	 * @param quantity How many times can a request be made within the time span given
+	 * @param time The time span
+	 * @param blockTime If the limit is reached for how much time block the ip
+	 */
+	constructor(
+		private quantity: number,
+		private time: number,
+		private blockTime: number
+	) {
+		// schedule cleanup on a regular interval (every 30 minutes)
+		setInterval(this.age, 30 * 60 * 1000);
+	}
+
+	private age() {
+		// clean up any accesses that have not been here within this.time and are not currently blocked
+		const now = Date.now();
+		const limit = now - this.time;
+
+		for (let ip in this.requests) {
+			const { accessTimes, blockUntil } = this.requests[ip];
+
+			// if not currently blocking this one
+			if (blockUntil >= now) continue;
+
+			// if newest access is older than time limit, then nuke the whole item
+			if (!accessTimes.length || accessTimes[accessTimes.length - 1] < limit) {
+				delete this.requests[ip];
+			} else {
+				// in case an ip is regularly visiting so its recent access is never old
+				// we must age out older access times to keep them from
+				// accumulating forever
+				if (accessTimes.length > this.quantity * 2 && accessTimes[0] < limit) {
+					let index = 0;
+					for (let i = 1; i < accessTimes.length; i++) {
+						if (accessTimes[i] < limit) {
+							index = i;
+						} else {
+							break;
+						}
+					}
+
+					// remove index + 1 old access times from the front of the array
+					accessTimes.splice(0, index + 1);
+				}
+			}
+		}
+	}
+	private add(ip: string) {
+		if (!this.requests[ip]) this.requests[ip] = { accessTimes: [], blockUntil: 0 };
+
+		// push this access time into the access array for this IP
+		this.requests[ip].accessTimes.push(Date.now());
+	}
+	public check(ip: string) {
+		// add this access
+		this.add(ip);
+
+		// should always be an info here because we just added it
+		const { accessTimes, blockUntil } = this.requests[ip];
+		// calc time limits
+		const now = Date.now();
+		const limit = now - this.time;
+		let count = 0;
+
+		// short circuit if already blocking this ip
+		if (blockUntil >= now) return false;
+		// short circuit an access that has not even had max quantity accesses yet
+		if (accessTimes.length < this.quantity) return true;
+
+		for (let i = accessTimes.length - 1; i >= 0; i--) {
+			if (accessTimes[i] > limit) {
+				++count;
+				continue;
+			}
+
+			// assumes counts are in time order so no need to look any more
+			break;
+		}
+
+		if (count > this.quantity) {
+			// block from now until (now + this.blockTime)
+			this.requests[ip].blockUntil = now + this.blockTime;
+			return false;
+		}
+
+		return true;
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,51 @@ import express from "express";
 import path from "node:path";
 import monitor from "express-status-monitor";
 import _ from "lodash";
+
 import CountryStateCityHandler from "./api/country-state-city";
 import CacheHandler from "./cache";
+import AccessLogger from "./access";
+import { makeJsonResponse } from "./utils";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const requestQuantity = Number(process.env.REQUEST_QUANTITY) || 9;
+const requestTime = Number(process.env.REQUEST_TIME) || 3000;
+const requestBlockTime = Number(process.env.REQUEST_BLOCK_TIME) || 10000;
+/** Request chache in seconds */
+const requestCache = Number(process.env.REQUEST_CACHE) || 60 * 60 * 24;
+/** 9 request every 3 second, then wait for 10 seconds */
+const logger = new AccessLogger(requestQuantity, requestTime, requestBlockTime);
 
+/** Allow monitoring '/status' */
 if (process.env.DEBUG) app.use(monitor());
+
+/** Limits the number of request from any ip */
+app.use(function (req, res, next) {
+	const JsonResponse = makeJsonResponse(res);
+
+	if (!logger.check(req.ip)) {
+		// cancel the request here
+		return JsonResponse("Too many requests", 429, { "Retry-After": requestBlockTime });
+	}
+	next();
+});
 
 /**
  * Public
+ *
+ * @static
  */
 app.use(express.static(path.join(__dirname, "../public")));
 
 /**
- * Countries endpoint
- * TODO: add property filters
+ * Countries endpoint, data by country
  *
- * Data by country
+ * @endpoint
  */
-app.get("/api/v1/:country?/:state?/:city?", CacheHandler(60), CountryStateCityHandler);
+app.get("/api/v1/:country?/:state?/:city?", CacheHandler(requestCache), CountryStateCityHandler);
 
+/** Boot server up */
 app.listen(port, () => {
 	if (process.env.DEBUG) console.log(`Listening on http://localhost:${port}/`);
 });
