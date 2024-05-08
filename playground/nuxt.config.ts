91373564c6b971c20e2d66d7123f3b49dc6f47ae
Victor Saa <vis97c@hotmail.com>
2024-05-08 12:25:19 -0500

    fix: allow crawler indexing

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
 			meta: [
 				{ charset: "utf-8" },
 				{ name: "viewport", content: "width=device-width, initial-scale=1" },
-				{ name: "robots", content: "noindex, nofollow" },
+				{ name: "robots", content: "index, follow" },
 			],
 			link: [
 				{
